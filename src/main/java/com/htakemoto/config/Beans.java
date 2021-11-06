8793ed2014d445e1db4e34b5d6747160837f6ce9
sergioK <borisfa88@gmail.com>
2021-11-06 10:53:19 +0200

    Init

--- src/main/java/com/htakemoto/config/Beans.java
+++ src/main/java/com/htakemoto/config/Beans.java
@@ -1,7 +1,5 @@
 package com.htakemoto.config;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +11,5 @@ public class Beans {
         return new AccessLog();
     }
 	
-    @Bean
-    public ModelMapper pojoMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-        return modelMapper;
-    }
+
 }
